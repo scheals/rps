94d93165dadb4a38cb6b3bb2eb48dc66d919f225
scheals <piecoo@gmail.com>
2021-12-19 23:32:06 +0100

    Make functions return something

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!")
-
 function game(){
     let computerScore = 0;
     let playerScore = 0;
@@ -9,43 +7,66 @@ function game(){
         let sign = signs[Math.floor(Math.random() * 3)]
         return sign
     }
+
     function playRound (playerSelection, computerSelection) {
         console.log(playerSelection)
         console.log(computerSelection)
         if (playerSelection === computerSelection) {
         computerScore++
         playerScore++
-        alert(`You chose the same sign! That's a draw. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `You chose the same sign! That's a draw. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
+        } else if (playerSelection === null) {
+        let result = `Those are not the droids you are looking for!`;
+        return result;
         } else if (playerSelection === "rock" && computerSelection === "scissors"){
         playerScore++
-        alert(`Congratulations! Rock beats Scissors. You've won. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `Congratulations! Rock beats Scissors. You've won. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
         } else if (playerSelection === "paper" && computerSelection === "rock"){
         playerScore++
-        alert(`Congratulations! Paper beats Rock. You've won. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `Congratulations! Paper beats Rock. You've won. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
         } else if (playerSelection === "scissors" && computerSelection === "paper"){
         playerScore++
-        alert(`Congratulations! Scissors beat Paper. You've won. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `Congratulations! Scissors beat Paper. You've won. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
         } else if (playerSelection === "rock" && computerSelection === "paper"){
         computerScore++
-        alert(`Whoopsie! Rock got beaten by Paper. You've lost. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `Whoopsie! Rock got beaten by Paper. You've lost. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
         } else if (playerSelection === "paper" && computerSelection === "scissors"){
         computerScore++
-        alert(`Whoopsie! Paper got beaten by Scissors. You've lost. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `Whoopsie! Paper got beaten by Scissors. You've lost. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
         } else if (playerSelection === "scissors" && computerSelection === "rock"){
         computerScore++
-        alert(`Whoopsie! Scissors got beaten by Rock. You've lost. Current score: You ${playerScore} AI ${computerScore}`)
+        let result = `Whoopsie! Scissors got beaten by Rock. You've lost. Current score: You ${playerScore} AI ${computerScore}`;
+        console.log(result);
+        return result;
         }
         else {
-        alert("Only rocks, paper and scissors accepted!")}
+        let result = "Only rocks, paper and scissors accepted!";
+        console.log(result);
+        return result;
+        }
+    }
 
-    // if (playerScore >= 5 || computerScore >= 5) {
-    //     } else if (playerScore === computerScore){
-    //     alert(`Huh! You tied. Interesting! Your final score is: You ${playerScore} AI ${computerScore}`)
-    //     } else if (playerScore > computerScore) {
-    //         alert(`Congratulations! You've won. Your final score is: You ${playerScore} AI ${computerScore}`)
-    //     } else {
-    //         alert(`Oh shoot... You've lost. Yoru final score is: You ${playerScore} AI ${computerScore}`)
-    //     }
+    function playFive () {
+        if (playerScore >= 5 || computerScore >= 5) {
+        }else if (playerScore === computerScore){
+        alert(`Huh! You tied. Interesting! Your final score is: You ${playerScore} AI ${computerScore}`)
+        } else if (playerScore > computerScore) {
+        alert(`Congratulations! You've won. Your final score is: You ${playerScore} AI ${computerScore}`)
+        } else {
+        alert(`Oh shoot... You've lost. Your final score is: You ${playerScore} AI ${computerScore}`)
+        }
     
     }
     playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
@@ -63,7 +84,6 @@ function game(){
     playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
     console.log(playerScore);
     console.log(computerScore);
+    playFive()
 }
-
-
 game();

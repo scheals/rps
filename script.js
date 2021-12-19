8e3e1c7072b701d527fb3fbe7ee45aaabb605d2f
scheals <piecoo@gmail.com>
2021-12-19 22:42:53 +0100

    Implement multi round

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
 console.log("Hello World!")
 
-function computerPlay () {
-    const signs = ["rock", "paper", "scissors"]
-    let sign = signs[Math.floor(Math.random() * 3)]
-    return sign
-}
-let computerSelection = computerPlay()
-console.log(computerSelection)
-let playerSelection = prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase()
-console.log(playerSelection)
-function oneRound () {
-    if (playerSelection === computerSelection) {
-        alert("Wow! You chose the same sign! That's a draw.")
-    } else if (playerSelection === "rock" && computerSelection === "scissors"){
-        alert("Congratulations! Rock beats Scissors. You've won.")
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
-        alert("Congratulations! Paper beats Rock. You've won.")
-    } else if (playerSelection === "scissors" && computerSelection === "paper"){
-        alert("Congratulations! Scissors beat Paper. You've won.")
-    } else if (playerSelection === "rock" && computerSelection === "paper"){
-        alert("Whoopsie! Rock got beaten by Paper. You've lost.")
-    } else if (playerSelection === "paper" && computerSelection === "scissors"){
-        alert("Whoopsie! Paper got beaten by Scissors. You've lost.")
-    } else if (playerSelection === "scissors" && computerSelection === "rock"){
-        alert("Whoopsie! Scissors got beaten by Rock. You've lost.")
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+
+    function computerPlay () {
+        const signs = ["rock", "paper", "scissors"]
+        let sign = signs[Math.floor(Math.random() * 3)]
+        return sign
     }
-    else {
+    function playRound (playerSelection, computerSelection) {
+        console.log(playerSelection)
+        console.log(computerSelection)
+        if (playerSelection === computerSelection) {
+        computerScore++
+        playerScore++
+        alert(`You chose the same sign! That's a draw. Current score: You ${playerScore} AI ${computerScore}`)
+        } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        playerScore++
+        alert(`Congratulations! Rock beats Scissors. You've won. Current score: You ${playerScore} AI ${computerScore}`)
+        } else if (playerSelection === "paper" && computerSelection === "rock"){
+        playerScore++
+        alert(`Congratulations! Paper beats Rock. You've won. Current score: You ${playerScore} AI ${computerScore}`)
+        } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore++
+        alert(`Congratulations! Scissors beat Paper. You've won. Current score: You ${playerScore} AI ${computerScore}`)
+        } else if (playerSelection === "rock" && computerSelection === "paper"){
+        computerScore++
+        alert(`Whoopsie! Rock got beaten by Paper. You've lost. Current score: You ${playerScore} AI ${computerScore}`)
+        } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        computerScore++
+        alert(`Whoopsie! Paper got beaten by Scissors. You've lost. Current score: You ${playerScore} AI ${computerScore}`)
+        } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        computerScore++
+        alert(`Whoopsie! Scissors got beaten by Rock. You've lost. Current score: You ${playerScore} AI ${computerScore}`)
+        }
+        else {
         alert("Only rocks, paper and scissors accepted!")}
+
+    // if (playerScore >= 5 || computerScore >= 5) {
+    //     } else if (playerScore === computerScore){
+    //     alert(`Huh! You tied. Interesting! Your final score is: You ${playerScore} AI ${computerScore}`)
+    //     } else if (playerScore > computerScore) {
+    //         alert(`Congratulations! You've won. Your final score is: You ${playerScore} AI ${computerScore}`)
+    //     } else {
+    //         alert(`Oh shoot... You've lost. Yoru final score is: You ${playerScore} AI ${computerScore}`)
+    //     }
+    
+    }
+    playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
+    console.log(playerScore);
+    console.log(computerScore);
+    playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
+    console.log(playerScore);
+    console.log(computerScore);
+    playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
+    console.log(playerScore);
+    console.log(computerScore);
+    playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
+    console.log(playerScore);
+    console.log(computerScore);
+    playRound(prompt("Choose wisely!", "Rock, Paper or Scissors?").toLowerCase(), computerPlay());
+    console.log(playerScore);
+    console.log(computerScore);
 }
-oneRound();
+
+
+game();

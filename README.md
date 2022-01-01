76add79703db0252666d73c50fd313685033dd47
scheals <piecoo@gmail.com>
2022-01-01 21:56:15 +0100

    Make game playable through buttons

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rps
 Third project from TOP - Rock Paper Scissors 
+Revisiting it with UI possible thanks to JS.
--- script.js
+++ script.js
@@ -105,4 +105,103 @@ function game(){
 playRound()
 playFive()
 }
-game();
+
+let computerScore = 0;
+let playerScore = 0;
+let roundCount = 0;
+let result = "Click on one of the buttons to pick your sign!"
+function getAISign () {
+    const signs = ["rock", "paper", "scissors"]
+    let sign = signs[Math.floor(Math.random() * 3)]
+    return sign
+}
+
+function playRound (playerSelection, computerSelection = getAISign ()) {
+    console.log(computerSelection);
+    if (playerSelection === computerSelection) {
+    roundCount++;    
+    let result = `You chose the same sign! That's a draw. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    } else if (playerSelection === undefined) {
+    let playerInput = prompt("Choose wisely!", "Rock, Paper or Scissors?")
+        if (playerInput === null) {
+        let result = "Only rocks, paper and scissors accepted!"
+        results.textContent = result;
+        return result
+        } else {
+        let playerSelection = playerInput.toLowerCase()
+        console.log(playerSelection)
+        return playRound(playerSelection); 
+    }
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+    playerScore++;
+    roundCount++;
+    let result = `Congratulations! Rock beats Scissors. You've won. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+    playerScore++;
+    roundCount++;
+    let result = `Congratulations! Paper beats Rock. You've won. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+    playerScore++;
+    roundCount++;
+    let result = `Congratulations! Scissors beat Paper. You've won. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+    computerScore++;
+    roundCount++;;
+    let result = `Whoopsie! Rock got beaten by Paper. You've lost. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+    computerScore++;
+    roundCount++;
+    let result = `Whoopsie! Paper got beaten by Scissors. You've lost. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+    computerScore++;
+    roundCount++;
+    let result = `Whoopsie! Scissors got beaten by Rock. You've lost. Current score: You ${playerScore} AI ${computerScore}`;
+    results.textContent = result;
+    return result;
+    }
+    else {
+    let result = "Only rocks, paper and scissors accepted!";
+    results.textContent = result;
+    return result;
+    }
+}
+
+function playFive () {
+    console.log(roundCount)
+    if (roundCount >= 5) {
+        if (playerScore === computerScore){
+                alert(`Huh! You tied. Interesting! Your final score is: You ${playerScore} AI ${computerScore}`) 
+            } else if (playerScore > computerScore) {
+                alert(`Congratulations! You've won. Your final score is: You ${playerScore} AI ${computerScore}`)
+            } else {
+            alert(`Oh shoot... You've lost. Your final score is: You ${playerScore} AI ${computerScore}`)
+        }
+        }else {
+            console.log(`Five rounds not played yet.`)
+    }
+    
+}
+
+
+function attachPlayRound(element){
+    return element.addEventListener('click', function(){playRound(`${element.dataset.sign}`)});
+    
+}
+const playButtons = document.querySelectorAll(".play-btn");
+playButtons.forEach(element => attachPlayRound(element));
+console.log(playButtons);
+
+const results = document.querySelector(".results");
+results.textContent = result;
